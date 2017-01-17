d74ca8f08e8757a02bffd3ae22155dbe078cda26
Kaleb Haslam <kalebmarc@hotmail.com>
2017-01-16 17:41:58 -0800

    removed metasploit from setup

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -5,11 +5,6 @@ sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
 sudo test
-sudo apt-get install postgresql
-sudo apt-get install curl
-sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-chmod 755 msfinstall && \
-./msfinstall
 sudo apt-get install sl
 sudo apt-get install whois
 sudo apt-get install httrack
