3f93c2796d4c8cebf87e91bc40ad066d568bbb5e
Kaleb Haslam <hhaslam11@users.noreply.github.com>
2017-11-12 04:45:36 -0800

    Assume-yes for installing things

--- Scripts/vnc_deb.sh
+++ Scripts/vnc_deb.sh
@@ -1,13 +1,13 @@
 #Meant for debian
 #Based off of https://www.digitalocean.com/community/tutorials/how-to-set-up-vnc-server-on-debian-8
-apt-get update
+apt-get -y update
 apt-get -y upgrade
-apt-get update
-apt-get install xfce4 xfce4-goodies gnome-icon-theme tightvncserver
-apt-get install iceweasel
+apt-get -y update
+apt-get -y install xfce4 xfce4-goodies gnome-icon-theme tightvncserver
+apt-get -y install iceweasel
 adduser vnc
-apt-get update
-apt-get install sudo
+apt-get -y update
+apt-get install -y sudo
 gpasswd -a vnc sudo
 su - vnc
 clear
