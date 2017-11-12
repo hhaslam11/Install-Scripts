e36b7b2b398576d780d8122272f8a83374c9c3e6
Kaleb Haslam <kalebmarc@hotmail.com>
2017-11-12 12:26:04 +0000

    Made server_setup not prompt user

--- Scripts/setup_server.sh
+++ Scripts/setup_server.sh
@@ -1,17 +1,17 @@
 cd /
-sudo apt-get update
-sudo apt-get install apache2
-sudo apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo apt-get -y update
+sudo apt-get install -y apache2
+sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 sudo echo "<IfModule mod_dir.c>
 
           DirectoryIndex index.php index.html index.cgi index.pl index.php index.xhtml index.htm
 
 </IfModule>" >> /etc/apache2/mods-enabled/dir.conf
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 npm install --global gulp
-sudo apt-get autoremove
+sudo apt-get -y autoremove
 clear
