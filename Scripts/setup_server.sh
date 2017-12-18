349d009373ad7450a2e64f8b7ee031a4aa6eb6de
Kaleb Haslam <hhaslam11@users.noreply.github.com>
2017-12-17 18:46:27 -0800

    Changed from php5 to generic php

--- Scripts/setup_server.sh
+++ Scripts/setup_server.sh
@@ -1,10 +1,10 @@
 cd /
 sudo apt-get -y update
 sudo apt-get install -y apache2
-sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php-mysql
 sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
-sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php libapache2-mod-php php-mcrypt
 sudo echo "<IfModule mod_dir.c>
 
           DirectoryIndex index.php index.html index.cgi index.pl index.php index.xhtml index.htm
