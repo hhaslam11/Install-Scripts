f2b06018172e6b1c3ca8dcca693ef4206ad1945d
Kaleb Haslam <kalebmarc@hotmail.com>
2018-05-26 20:51:10 -0700

    added bitnami.sh

---
+++ scripts/bitnami.sh
@@ -0,0 +1,4 @@
+wget https://bitnami.com/redirect/to/167501/bitnami-lampstack-7.1.12-0-linux-x64-installer.run
+sudo chmod 0755 bitnami-lampstack-7.1.12-0-linux-x64-installer.run
+sudo ./bitnami-lampstack-7.1.12-0-linux-x64-installer.run
+rm bitnami-lampstack-7.1.12-0-linux-x64-installer.run
